32505a79726a43139f4486c83ecf4389cd26bcfe
mianfahad4884 <mianf4884@gmail.com>
2026-04-07 08:25:46 +0500

    Configure Android build via GitHub Actions

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.noor.project',
+  appName: 'Project',
+  webDir: 'out'
+};
+
+export default config;
